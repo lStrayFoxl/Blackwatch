86b889c567e7e4b07357db8995e32efbc7afdfbe
StrayFox <strayfox1@mail.ru>
2022-10-10 20:19:33 +0300

    Made markup and styles in index.php

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blackwatch</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="countainer">
+
+            <div class="hat">
+                <h2 class="article_hat">Black and <span class="yellow">yellow</span></h2>
+                <div class="linkHat">
+                    <a href="#models" class="link">MODELS</a>
+                    <a href="#purchase" class="link">PURCHASE</a>
+                    <a href="#" class="link">ABOUT US</a>
+                </div>
+            </div>
+
+            <div class="header_content">
+                <div class="hatDescript">
+                    <h1 class="articleHat">BLACKWATCH</h1>
+                    <div class="descript">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus metus leo, 
+                        aliquam nec ex sed, suscipit cursus lectus. Fusce dapibus cursus tristique. Vestibulum ex erat.
+                    </div>
+                </div>
+                <div class="blackwatch_main"></div>
+            </div>
+        
+        </div>
+    </header>
+
+    <main class="mainSection">
+
+        <section class="aboutTheWatch">
+            <div class="countainer">
+                
+                <h3 class="article">ABOUT THE WATCH</h3>
+                <div class="descriptTheWatch">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue velit lectus, sed faucibus 
+                    elit commodo at. Nulla facilisi. Nunc scelerisque nisl elementum elit bibendum, vel feugiat sapien elementum. 
+                    Sed in mauris ut libero dignissim posuere eget et ante. Nunc vitae aliquam arcu. Donec pharetra enim vel vehicula 
+                    vestibulum. Pellentesque malesuada risus ut augue lacinia gravida. Nullam tristique suscipit arcu ut tempus. Ut leo 
+                    odio, tempus at magna nec, luctus consequat urna. Morbi a nisl tellus. Nulla enim dui, iaculis in nulla eu, pellentesque 
+                    ultrices lectus. In sed pharetra neque, non ultrices erat. Ut ex odio, imperdiet sit amet rhoncus at, facilisis non ligula. 
+                    Donec vitae consectetur nulla.
+                </div>
+                <div class="photo_watch"></div>
+
+            </div>
+        </section>
+
+        <section class="models" id="models">
+            <div class="countainer">
+
+                <h3 class="article">MODELS</h3>
+                <div class="model-x">
+                    <div class="photoModelX"></div>
+                    <div class="title">BLACKWATCH-X</div>
+                    <div class="descriptModelX">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu mattis neque, in luctus enim. 
+                        Nulla facilisi. Nullam suscipit augue at commodo molestie. Ut tincidunt lectus ligula, vitae porttitor 
+                        ligula vulputate at. Nam a orci nunc. Quisque ut mi eu augue mattis lobortis nec ut ligula. Maecenas 
+                        augue magna, porta.
+                    </div>
+                </div>
+
+                <div class="model-y">
+                    <div class="photoModelY"></div>
+                    <div class="title">BLACKWATCH-Y</div>
+                    <div class="descriptModelY">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu mattis neque, in luctus enim. 
+                        Nulla facilisi. Nullam suscipit augue at commodo molestie. Ut tincidunt lectus ligula, vitae porttitor 
+                        ligula vulputate at. Nam a orci nunc. Quisque ut mi eu augue mattis lobortis nec ut ligula. Maecenas 
+                        augue magna, porta.
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+        <section class="purchase" id="purchase">
+            <div class="countainer">
+                
+                <h3 class="article">PURCHASE</h3>
+                <form action="#" method="get" class="form">
+                    <div class="inputFormInfo">
+                        <div class="collum1">
+                            <label for="enterName" class="labelForm">NAME</label><br>
+                            <input type="text" id="enterName" class="inputForm"><br><br>
+                            <label for="enterSurname" class="labelForm">SURNAME</label><br>
+                            <input type="text" id="enterSurname" class="inputForm"><br><br>
+                            <label for="enterPhone" class="labelForm">PHONE NUMBER</label><br>
+                            <input type="text" id="enterPhone" class="inputForm">
+                        </div>
+                        
+                        <div class="collum2">
+                            <label for="enterAddress" class="labelForm">ADDRESS</label><br>
+                            <input type="text" id="enterAddress" class="inputForm"><br><br>
+                            <label for="enterModel" class="labelForm">MODEL</label><br>
+                            <select id="enterModel" class="selectForm">
+                                <option selected class="none"></option>
+                                <option value="modelX" class="modelX">Blackwatch-X</option>
+                                <option value="modelY" class="modelY">Blackwatch-Y</option>
+                            </select><br>
+                        </div>
+                    </div>
+                    
+                    <button type="sumbit" class="butPurchase">PURCHASE</button>
+                </form>
+
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="footer">
+        <div class="countainer">
+
+            <div class="info">
+                <div class="articleInf">Information</div>
+                <a href="#models" class="linkFooter">MODELS</a><br><br>
+                <a href="#purchase" class="linkFooter">PURCHASE</a><br><br>
+                <a href="#" class="linkFooter">ABOUT US</a>
+            </div>
+
+            <div class="contact">
+                <div class="articleCont">Contact details</div>
+                <span class="number">+7956465-65-47</span>
+                <span class="number">+7956465-65-47</span><br><br>
+                <span class="number">+7956465-65-47</span>
+                <span class="number">+7956465-65-47</span>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
